20169f4d25e80f981615c14c57c4587fbce2e157
Scalent <priyanka.c@scalent.io>
2024-05-27 14:41:21 +0530

    multiplication changes

--- for-loop-in-golang/for-loop-with-nested-loop/main.go
+++ for-loop-in-golang/for-loop-with-nested-loop/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	size := 10
+	size := 5
 
 	// Outer loop for the rows
-	for i := 1; i <= size; i++ {
+	for row := 1; row <= size; row++ {
 		// Inner loop for the columns
-		for j := 1; j <= size; j++ {
-			fmt.Printf("%4d", i*j) // Print the product of i and j
+		for column := 1; column <= size; column++ {
+			fmt.Printf("%4d", row*column) // Print the product of i and j
 		}
 		fmt.Println() // Move to the next line after each row
 	}
