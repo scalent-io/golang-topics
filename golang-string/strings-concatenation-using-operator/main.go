d01d8de7278cd0dc3d2e14ea353e0dec95fced69
siddharth-scalent <siddharth@scalent.io>
2024-07-15 13:32:56 +0530

    comment has changed

--- golang-string/strings-concatenation-using-operator/main.go
+++ golang-string/strings-concatenation-using-operator/main.go
@@ -6,5 +6,5 @@ func main() {
 	message1 := "Golang has"
 	message2 := "built-in-concurrency support"
 	result := message1 + " " + message2
-	fmt.Println(result) // Output: Hello World
+	fmt.Println(result) // Golang has built-in-concurrency support
 }
