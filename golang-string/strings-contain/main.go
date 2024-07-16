b7d349d085aab830f5183776bce4dbe617b0297d
siddharth-scalent <141826568+siddharth-scalent@users.noreply.github.com>
2024-07-16 12:13:59 +0530

    Golang string (#19)

--- golang-string/strings-contain/main.go
+++ golang-string/strings-contain/main.go
@@ -9,8 +9,7 @@ func main() {
 	exist := strings.Contains("hello, world", "world")
 	if !exist {
 		fmt.Println("String does not contain the substring")
+	} else {
+		fmt.Println("String contains the substring")
 	}
-	fmt.Println(exist)
 }
-
-// output : true
